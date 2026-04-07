debf76fb34e6af5ffdb6565ad852eaa11452ea46
Москаленко Владислав <moskalvle273@gmail.com>
2026-04-07 08:05:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,46 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 #include <iterator>
 #include <algorithm>
-#include <string>
+
+struct WordComparator {
+    bool operator()(const std::string& a, const std::string& b) const {
+        if (a.length() != b.length()) {
+            return a.length() < b.length();
+        }
+        return a < b; 
+    }
+};
 
 int main() {
-    std::string name;
-    std::cout << "Введите имя файла: ";
-    std::cin >> name;
+    std::string name1, name2;
+    std::cout << "Введите имя первого файла: ";
+    std::cin >> name1;
+    std::cout << "Введите имя второго файла: ";
+    std::cin >> name2;
 
-    std::ifstream inputFile(name);
-    if (!inputFile.is_open()) {
-        std::cerr << "Ошибка: не удалось открыть файл " << name << std::endl;
+    std::ifstream file1(name1);
+    std::ifstream file2(name2);
+
+    if (!file1.is_open() || !file2.is_open()) {
+        std::cerr << "Ошибка при открытии файлов." << std::endl;
         return 1;
     }
+    std::istream_iterator<std::string> it1_begin(file1), it1_end;
+    std::istream_iterator<std::string> it2_begin(file2), it2_end;
+    std::ostream_iterator<std::string> out_it(std::cout, " ");
 
-    std::istream_iterator<int> start(inputFile);
-    std::istream_iterator<int> end;
-    std::ostream_iterator<int> out_it(std::cout, " ");
-
-    int counter = 0;
-
-    std::remove_copy_if(start, end, out_it, [&counter](int) {
-        counter++;
-        return (counter % 2 == 0); 
-    });
+    std::merge(it1_begin, it1_end, 
+               it2_begin, it2_end, 
+               out_it, 
+               WordComparator());
 
     std::cout << std::endl;
-    inputFile.close();
+
+    file1.close();
+    file2.close();
 
     return 0;
 }
